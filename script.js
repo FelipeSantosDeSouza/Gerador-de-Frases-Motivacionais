d5c032a317a0c5f76d6422e36768bc0ab42e02c9
Felipe Santos De Souza <felipestsantooss@gmail.com>
2025-11-09 21:49:42 -0300

    add css js

---
+++ script.js
@@ -0,0 +1,30 @@
+const frases= ["Acredite em si mesmo e tudo será possível 💪",
+  "Cada dia é uma nova chance de recomeçar 🌅",
+  "Pequenos passos também te levam longe 👣",
+  "Você é mais forte do que imagina 🦁",
+  "Grandes coisas começam com pequenos começos 🌱",
+  "Persistência é o caminho do êxito 🚀",
+  "A vitória nasce da superação diária 🏆",
+  "O sucesso é a soma de pequenos esforços 💼",
+  "Nunca é tarde para ser quem você poderia ser ✨",
+  "Seja a energia que você quer atrair 🔥"];
+
+
+  const frase = document.getElementById("frase")
+  const botao = document.getElementById("botao")
+
+
+  botao.addEventListener("click", function() {
+    const indice = Math.floor(Math.random() * frases.length);
+    frase.textContent=(frases[indice]);
+
+
+  const cor = `hsl(${Math.floor(Math.random() * 360)}, 70%, 50%)`;
+  document.getElementById("main").style.background = cor;
+    });
+
+    localStorage.setItem("ultimaFrase", JSON.stringify( frases[indice]));
+
+    const ultima = localStorage.getItem("ultimaFrase");
+if (ultima) frase.textContent = ultima;
+
